70b1f108345ad1784206d0557d8fcb2ca9160837
kumar <iosdeveloper.ipa@gmail.com>
2021-11-09 07:46:11 +0530

    Created supported class files for ViewControllers created in storyboard

--- UIObjects/UIObjects/ViewController1.swift
+++ UIObjects/UIObjects/ViewController1.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController1: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,6 @@ class ViewController: UIViewController {
 
 // MARK: - UIObjects
 
-extension ViewController {
+extension ViewController1 {
     
 }
---
+++ UIObjects/UIObjects/ViewController3.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController2.swift
+//  UIObjects
+//
+//  Created by Pavankumar Arepu on 09/11/2021.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
