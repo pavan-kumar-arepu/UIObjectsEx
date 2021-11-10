81b252e1e0e34f858fb069cac7f9a5587ba37e06
kumar <iosdeveloper.ipa@gmail.com>
2021-11-10 07:39:05 +0530

    SegmentControl changes pushed

--- UIObjects/UIObjects/ViewController1.swift
+++ UIObjects/UIObjects/ViewController1.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class ViewController1: UIViewController {
 
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        segmentControl.addTarget(self, action: #selector(segmentedValueChanged), for: .valueChanged)
+        segmentControl.selectedSegmentIndex = 2
+        segmentedValueChanged()
     }
 
+    // Add function to handle Value Changed events
+//    segmentControl.addTarget(self, action: #selector(self.segmentedValueChanged(_:)), for: .valueChanged)
 
+    @objc func segmentedValueChanged() {
+        let selectedSegment = segmentControl.selectedSegmentIndex
+        
+        switch selectedSegment {
+        case 0:
+            self.view.backgroundColor = UIColor.orange
+        case 1:
+            self.view.backgroundColor = UIColor.white
+        case 2:
+            self.view.backgroundColor = UIColor.green
+        default:
+            self.view.backgroundColor = UIColor.orange
+        }
+      }
 }
 
 
