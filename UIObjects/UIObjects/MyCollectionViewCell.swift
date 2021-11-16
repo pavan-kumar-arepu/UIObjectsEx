49124025c729a3ff19c6a3dd258287ed395c0427
kumar <iosdeveloper.ipa@gmail.com>
2021-11-16 07:53:51 +0530

    Session18: CollectionView Commit

---
+++ UIObjects/UIObjects/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  UIObjects
+//
+//  Created by Pavankumar Arepu on 16/11/2021.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var mySubLabel: UILabel!
+    
+}
--- UIObjects/UIObjects/ViewController4.swift
+++ UIObjects/UIObjects/ViewController4.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ViewController4: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var tableView: UITableView!
+    
+    let collectionViewReuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    
     var tableDataArray = ["General", "Accessibility", "Privacy", "Cricket", "Volleyball", "Hockey", "Movie", "Games", "Micky", "Mouse", "Jokkey", "Janson"]
 
     var imageArray = ["general","access","shield","cricket","cricket","cricket","cricket","cricket","cricket","cricket","cricket","cricket"]
+    
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    
+    
+    
+    // MARK: - View Controller methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,18 +30,12 @@ class ViewController4: UIViewController, UITableViewDelegate, UITableViewDataSou
         tableView.delegate = self
         tableView.dataSource = self
         // Do any additional setup after loading the view.
+        
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     
     // MARK: - TableView DataSource and Delegate methods
@@ -54,8 +58,45 @@ class ViewController4: UIViewController, UITableViewDelegate, UITableViewDataSou
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("Selected Tableview Cells is: ", tableDataArray[indexPath.row])
-        
     }
-    
 }
 
+// MARK: - CollectionView Datasource and Delegate methods
+
+extension ViewController4 : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewReuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+        
+        
+        // load the data in collection cell
+        
+        //cell.myLabel.text = self.items[indexPath.row] // The row value is the same as the index of the desired text within the array.
+        cell.myLabel.text = items[indexPath.row]
+        cell.mySubLabel.text = "Sub"
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+    // change background color when user touches cell
+    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.red
+    }
+    
+    
+    // change background color back when user releases touch
+    func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.cyan
+    }
+}
